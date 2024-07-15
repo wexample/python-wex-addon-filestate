8ad46205690827aaf420c5e870fffcf53405b203
weeger <romain.weeger@gmail.com>
2024-07-15 16:57:33 +0200

    Kernel directory structure

---
+++ wexample_wex_addon_app/const/globals.py
@@ -0,0 +1,9 @@
+from wexample_wex_core.const.globals import CORE_COMMAND_NAME
+
+APP_DIR_APP_DATA_NAME: str = f".{CORE_COMMAND_NAME}"
+APP_ENV_DEV: str = "dev"
+APP_ENV_LOCAL: str = "local"
+APP_ENV_PROD: str = "prod"
+APP_ENV_TEST: str = "test"
+APP_FILE_APP_CONFIG: str = "config.yml"
+APP_FILE_APP_ENV: str = ".env"
---
+++ wexample_wex_addon_app/utils/AppDirectoryStructure.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from wexample_filestate.const.enums import DiskItemType
+from wexample_filestate.const.types import StateItemConfig
+
+from wexample_wex_addon_app.const.globals import (
+    APP_FILE_APP_CONFIG,
+    APP_FILE_APP_ENV,
+)
+from wexample_wex_core.utils.workdir import Workdir
+
+
+class AppDirectoryStructure(Workdir):
+    pass
+
+    def build_setup_config(self, config: Optional[StateItemConfig] = None) -> StateItemConfig:
+        config = super().build_setup_config(config)
+
+        config["children"].append({
+            "name": APP_FILE_APP_CONFIG,
+            "type": DiskItemType.FILE,
+            "should_exist": True
+        })
+
+        config["children"].append({
+            "name": APP_FILE_APP_ENV,
+            "type": DiskItemType.DIRECTORY,
+            "should_exist": True,
+        })
+
+        config["children"].append({
+            "name": "tmp",
+            "type": DiskItemType.DIRECTORY,
+            "should_exist": True
+        })
+
+        return config
