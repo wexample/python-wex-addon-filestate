9d628ca16003d0e9cccc9750c29e8b36cc436d3a
weeger <romain.weeger@gmail.com>
2024-07-16 08:41:49 +0200

    Default readme

--- wexample_wex_addon_app/utils/AppDirectoryStructure.py
+++ wexample_wex_addon_app/utils/AppDirectoryStructure.py
@@ -1,7 +1,6 @@
-from typing import Optional
-
 from wexample_filestate.const.enums import DiskItemType
 from wexample_filestate.const.types import StateItemConfig
+from wexample_helpers.const.types import *
 
 from wexample_wex_addon_app.const.globals import (
     APP_FILE_APP_CONFIG,
@@ -11,7 +10,18 @@ from wexample_wex_core.utils.workdir import Workdir
 
 
 class AppDirectoryStructure(Workdir):
-    pass
+    def build_config(self, config: Optional[StateItemConfig] = None) -> StateItemConfig:
+        config = super().build_config(config)
+        children = config["children"]
+
+        children.append({
+            "name": 'README.md',
+            "type": DiskItemType.FILE,
+            "should_exist": True,
+            "default_content": f"# Readme"
+        })
+
+        return config or {}
 
     def build_setup_config(self, config: Optional[StateItemConfig] = None) -> StateItemConfig:
         config = super().build_setup_config(config)
